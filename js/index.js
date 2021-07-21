2ef888e8d213a347e44903276b627d7998190e7b
DrumPire <expresproject@gmail.com>
2021-07-21 20:46:40 +0300

    practice two

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
@@ -10,11 +10,28 @@ const personalMovieDB = {
   privat: false,
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++) {
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+  const lastWatchFilm = prompt('Один из последних просмотренных фильмов?', ''),
+        appraisal = prompt('На сколько оцените его?', '');
+
+  if (lastWatchFilm != null && appraisal != null && lastWatchFilm != '' && appraisal != '' && lastWatchFilm.length < 50) {
+    personalMovieDB.movies[lastWatchFilm] = appraisal;
+    console.log('Done!');
+  } else {
+    console.log('Error!');
+    i--;
+  }
+}
+
+if (personalMovieDB.count < 10) {
+  console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count === 10 && personalMovieDB.count <= 30) {
+  console.log('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+  console.log('Вы киноман');
+} else {
+  console.log('Произошла ошибка');
+}
+
 console.log(personalMovieDB);
