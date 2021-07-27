d6e1d6086e9ab295354123734b623cc379b47c1d
DrumPire <expresproject@gmail.com>
2021-07-27 21:50:58 +0300

    practice three

--- js/script.js
+++ js/script.js
@@ -1,31 +1,17 @@
-// function showFirstMessage(text) {
-//   console.log(text);
-// }
+'use strict';
 
-// showFirstMessage('hello world!');
-
-// const showFirstMessage = function() {
-//   console.log('Hello world!');
+const soldier = {
+  health: 400,
+  armor: 100,
+  sayHello: function() {
+    console.log('hello');
+  }
+};
+const john = Object.create(soldier);
+// const john = {
+//   health: 100
 // };
-
-// showFirstMessage();
-
-// const calc = (a, b) => a + b;
-
-// console.log(calc(5, 5));
-
-// const str = 'teSt';
-
-// console.log(str.toLowerCase());
-
-// const fruit = 'Some fruit';
-
-// console.log(fruit.slice(5, 10));
-// console.log(fruit.substring(5, 10));
-// console.log(fruit.substr(5, 6));
-
-const num = 12.2;
-console.log(Math.round(num));
-
-const test = '12.2px';
-console.log(parseFloat(test));
+// john.__proto__ = soldier;
+Object.setPrototypeOf(john, soldier);
+// console.log(john.armor);
+john.sayHello();
