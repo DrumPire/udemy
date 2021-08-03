47f7e7a0633d4270cd83dcce6d8848d7ce321a60
DrumPire <expresproject@gmail.com>
2021-08-03 14:21:22 +0300

    practice four

--- js/index.js
+++ js/index.js
@@ -54,6 +54,7 @@ const personalMovieDB = {
   writeYourGenres: function() {
     for (let i = 1; i <= 3; i++) {
       let genre = prompt(`Ваш любимый жанр под номером ${i}`, '');
+     
       if(genre === '' || genre == null) {
         console.log('Вы ввели не коректные данные или не ввели вовсе');
         i--;
@@ -61,6 +62,7 @@ const personalMovieDB = {
         personalMovieDB.genres[i - 1] = genre;
       }
     }
+    
     personalMovieDB.genres.forEach((item, i) => {
       console.log(`Любимый жанр ${i + 1} - это ${item}`);
     });
---
+++ project/js/script.js
@@ -0,0 +1,53 @@
+/* Задания на урок:
+
+1) Удалить все рекламные блоки со страницы (правая часть сайта)
+
+2) Изменить жанр фильма, поменять "комедия" на "драма"
+
+3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+Реализовать только при помощи JS
+
+4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Отсортировать их по алфавиту 
+
+5) Добавить нумерацию выведенных фильмов */
+
+'use strict';
+const promoBlock = document.querySelectorAll('.promo__adv img'),
+      posterBg = document.querySelector('.promo__bg'),
+      filmGenre = posterBg.querySelector('.promo__genre'),
+      movieList = document.querySelector('.promo__interactive-list');
+
+
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
+};
+
+// 1
+promoBlock.forEach(item => {
+    item.remove();
+});
+// 2
+filmGenre.textContent = 'драма';
+// 3
+posterBg.style.backgroundImage = 'url("img/bg.jpg")';
+// 4
+movieList.innerHTML = '';
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `
+    <li class="promo__interactive-item">${i + 1} ${film}
+        <div class="delete"></div>
+    </li>
+    `;
+});
+
+
+
