ba63ddbad7e61e4c263505436e7a939c3a8d9add
DrumPire <expresproject@gmail.com>
2021-08-03 20:28:41 +0300

    practice five

--- js/script.js
+++ js/script.js
@@ -1,17 +1,37 @@
 'use strict';
 
-const soldier = {
-  health: 400,
-  armor: 100,
-  sayHello: function() {
-    console.log('hello');
-  }
-};
-const john = Object.create(soldier);
-// const john = {
-//   health: 100
+// const soldier = {
+//   health: 400,
+//   armor: 100,
+//   sayHello: function() {
+//     console.log('hello');
+//   }
 // };
-// john.__proto__ = soldier;
-Object.setPrototypeOf(john, soldier);
-// console.log(john.armor);
-john.sayHello();
+// const john = Object.create(soldier);
+// // const john = {
+// //   health: 100
+// // };
+// // john.__proto__ = soldier;
+// Object.setPrototypeOf(john, soldier);
+// // console.log(john.armor);
+// john.sayHello();
+
+// console.log(document.head);
+// console.log(document.documentElement);
+// console.log(document.body.childNodes);
+// console.log(document.body.firstChild);
+// console.log(document.body.lastElementChild);
+// console.log(document.body.lastChild);
+
+// console.log(document.querySelector('#current').parentNode.parentNode);
+// console.log(document.querySelector('#current').parentElement.parentElement);
+// console.log(document.querySelector('[data-current="3"]').nextElementSibling);
+
+for (let node of document.body.childNodes) {
+  
+  if(node.nodeName === '#text') {
+    continue;
+  }
+  
+  console.log(node);
+}
--- project/js/script.js
+++ project/js/script.js
@@ -1,25 +1,32 @@
 /* Задания на урок:
 
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
+1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
 
-2) Изменить жанр фильма, поменять "комедия" на "драма"
+2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
 
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
 
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+"Добавляем любимый фильм"
 
-5) Добавить нумерацию выведенных фильмов */
+5) Фильмы должны быть отсортированы по алфавиту */
 
 'use strict';
-const promoBlock = document.querySelectorAll('.promo__adv img'),
-      posterBg = document.querySelector('.promo__bg'),
-      filmGenre = posterBg.querySelector('.promo__genre'),
-      movieList = document.querySelector('.promo__interactive-list');
+document.addEventListener('DOMContentLoaded', () => {
+    const adv = document.querySelectorAll('.promo__adv img'),
+    posterBg = document.querySelector('.promo__bg'),
+    filmGenre = posterBg.querySelector('.promo__genre'),
+    movieList = document.querySelector('.promo__interactive-list'),
+    addForm = document.querySelector('form.add'),
+    addInput = addForm.querySelector('.adding__input'),
+    checkbox = addForm.querySelector('[type="checkbox"]'),
+    btn = document.querySelector('button');
 
-
-const movieDB = {
+    const movieDB = {
     movies: [
         "Логан",
         "Лига справедливости",
@@ -27,26 +34,77 @@ const movieDB = {
         "Одержимость",
         "Скотт Пилигрим против..."
     ]
-};
+    };
+    
+    const sortArr = arr => {
+        arr.sort();
+    };
 
-// 1
-promoBlock.forEach(item => {
-    item.remove();
-});
-// 2
-filmGenre.textContent = 'драма';
-// 3
-posterBg.style.backgroundImage = 'url("img/bg.jpg")';
-// 4
-movieList.innerHTML = '';
-movieDB.movies.sort();
+    sortArr(movieDB.movies);
 
-movieDB.movies.forEach((film, i) => {
-    movieList.innerHTML += `
-    <li class="promo__interactive-item">${i + 1} ${film}
-        <div class="delete"></div>
-    </li>
-    `;
+    function createMovieList(films, parent) {
+        parent.innerHTML = '';
+        sortArr(films);
+        
+        films.forEach((film, i) => {
+            parent.innerHTML += `
+        <li class="promo__interactive-item">${i + 1} ${film}
+            <div class="delete"></div>
+        </li>
+        `;
+
+        document.querySelectorAll('.delete').forEach((btn, i) => {
+            btn.addEventListener('click', () => {
+                btn.parentElement.remove();
+                movieDB.movies.splice(i, 1);
+
+                createMovieList(films, parent);
+            });
+        });    
+
+        });
+    }
+
+    const deleteAdv = arr => {
+        arr.forEach(item => {
+            item.remove();
+            });
+    };
+
+    const makeChanges = () => {
+        filmGenre.textContent = 'драма';
+
+        posterBg.style.backgroundImage = 'url("img/bg.jpg")';
+    };
+
+    addForm.addEventListener('submit', e => {
+    e.preventDefault();
+    
+    let newFilm = addInput.value;
+    const favorite = checkbox.checked;
+
+    if(newFilm) {
+
+        if(newFilm.length > 21) {
+            newFilm = `${newFilm.substring(0, 22)}...`;
+        }
+
+        if(favorite) {
+            console.log('Добавляем любимый фильм');
+        }
+
+        movieDB.movies.push(newFilm);  
+        sortArr(movieDB.movies);
+    
+        createMovieList(movieDB.movies, movieList);
+    }
+    e.target.reset(); 
+    });
+
+    deleteAdv(adv);
+    makeChanges();
+    createMovieList(movieDB.movies, movieList);
+
 });
 
 
